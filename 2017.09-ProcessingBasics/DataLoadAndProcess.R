958b73b30187be536763a1f87ff2ac1a21ebaca1
David Thole <dthole@gmail.com>
2017-11-13 11:50:37 -0600

    Adding stubs for each of the questions, adding a load of the data from the raw site, vs reading locally

--- 2017.09-ProcessingBasics/DataLoadAndProcess.R
+++ 2017.09-ProcessingBasics/DataLoadAndProcess.R
@@ -9,3 +9,17 @@ library("ggplot2")
 library("gridExtra")
 library("dtplyr")
 library("dplyr")
+
+csvData <- read.table("https://raw.githubusercontent.com/TheDarkTrumpet/RUserGroup/master/2017.09-ProcessingBasics/Data/CCWS-IQ_sensor_data_6-26-17_10-16-17-all.csv", header=TRUE, sep=",")
+
+summary(csvData)
+
+# Average Moisture - per day
+
+# Average Humidity - per day
+
+# Average Atmospheric Pressure - per day
+
+# Rainy Day - average moisture per hour
+
+# Detect days right before it rains (atmospheric pressure drop)
